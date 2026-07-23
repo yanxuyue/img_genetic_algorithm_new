8f16054d834ea394bc799d3281b8047c05bbfc3f
yanrenjie <2602736374@qq.com>
2026-07-23 17:44:45 +0800

    加上了首尾约束和2-opt

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy.ndimage import median_filter
 
 # 图片数组
 pics = np.array(["./B_img/000.bmp", "./B_img/001.bmp", "./B_img/002.bmp", "./B_img/003.bmp", "./B_img/004.bmp",
@@ -15,9 +16,10 @@ picspixel = np.zeros((19, 2), dtype=object)
 for i in range(19):
     x = Image.open(f"{pics[i]}")
     y = np.array(x.convert("L"))  # 变成像素矩阵
-    # ------二值化 ------
-    y = (y > 128).astype(int)
-    # ------二值化结束----
+    # ------ 二值化改进（文字=1，背景=0，去噪） ------
+    y = median_filter(y, size=3)  # 中值滤波去噪
+    y = (y < 128).astype(int)  # 黑字变1，白背景变0
+    # ---------------------------------------------
     picspixel[i, 0] = y[:, 0]
     picspixel[i, 1] = y[:, -1]
 
@@ -26,7 +28,16 @@ diftable = np.zeros((19, 19))
 for i in range(19):
     for j in range(19):
         diftable[i, j] = np.sum(abs(picspixel[i, 1] - picspixel[j, 0]))
-
+# ------ 首尾纸片识别与约束（用原始灰度图，避免二值化干扰） ------
+left_sums = [np.sum(np.array(Image.open(f"{pics[i]}").convert("L"))[:, 0]) for i in range(19)]
+right_sums = [np.sum(np.array(Image.open(f"{pics[i]}").convert("L"))[:, -1]) for i in range(19)]
+left_white = np.argmax(left_sums)    # 最左纸片
+right_white = np.argmax(right_sums)  # 最右纸片
+# 软引导（替代硬约束）
+penalty = 100  # 相当于100个像素完全错位的代价，具体数值可微调
+diftable[:, left_white] += penalty   # 最左纸片被接在后面会被惩罚
+diftable[right_white, :] += penalty  # 最右纸片接在前面会被惩罚
+# ----------------------------------------------------------
 # 初始种群,允许重复的排列
 n = 100  # 族群内的个数,用偶数
 first_generation = np.zeros((n, 19), dtype=int)
@@ -41,6 +52,31 @@ for i in range(n):
         sums += diftable[first_generation[i][j], first_generation[i][j+1]]
     fitness[i] = 1/(sums+1e-8)
 
+# ---------------------------------------------
+def two_opt(individual, diftable):
+    """对个体（纸片排列）进行2-opt局部搜索，直到无法改进为止"""
+    improved = True
+    n = len(individual)
+    best_cost = sum(diftable[individual[k], individual[k+1]] for k in range(n-1))
+    while improved:
+        improved = False
+        for i in range(n-1):
+            for j in range(i+2, n):
+                # 当前边： (i, i+1) 和 (j, j+1)，注意 j 可能为 n-1
+                old_cost = diftable[individual[i], individual[i+1]]
+                if j < n-1:
+                    old_cost += diftable[individual[j], individual[j+1]]
+                # 新边： (i, j) 和 (i+1, j+1)
+                new_cost = diftable[individual[i], individual[j]]
+                if j < n-1:
+                    new_cost += diftable[individual[i+1], individual[j+1]]
+                # 如果新连接代价更小，则执行逆转
+                if new_cost < old_cost:
+                    individual[i+1:j+1] = individual[i+1:j+1][::-1]
+                    best_cost += (new_cost - old_cost)
+                    improved = True
+    return individual
+# ---------------------------------------------
 
 # 初始化
 iteration = 0  # 迭代进程
@@ -50,7 +86,7 @@ difbest = np.zeros(iterations)  # 每代最优代价
 pathbest = np.zeros((iterations, 19))  # 每代最优排序
 bestpath = np.zeros(19)  # 全局最优解
 mutation_p = 0.01  # 变异概率,小于才变
-span_num = 40  # 逆转次数
+span_num = 50  # 逆转次数
 
 while iteration < iterations:
     sons = np.zeros((n, 19), dtype=int)  # 暂时存储每一代
@@ -127,6 +163,17 @@ while iteration < iterations:
         for j in range(18):
             c += diftable[sons[i][j], sons[i][j+1]]
         temdif[i] = c
+    # -------对当代最优个体进行2-opt-----------
+    best_idx = np.argmin(temdif)
+    optimized = two_opt(sons[best_idx].copy(), diftable)
+
+    # 如果2-opt确实改进了，就替换回去
+    opt_cost = sum(diftable[optimized[k], optimized[k + 1]] for k in range(18))
+    if opt_cost < temdif[best_idx]:
+        sons[best_idx] = optimized
+        temdif[best_idx] = opt_cost
+        son_fitness[best_idx] = 1 / (opt_cost + 1e-8)
+    # ---------------------------------------
     # 每代平均代价
     difmean[iteration] = np.mean(temdif)
     # 每代最优代价
@@ -153,7 +200,7 @@ ax[1].plot(np.arange(iteration), difbest, 'k')
 ax[1].set_title('Best Difference')
 ax[1].set_xlabel('Iteration')
 
-fig.savefig("dif_04.png")
+fig.savefig("dif_07.png")
 plt.show()
 
 # 拼接纸片(横向)
@@ -165,6 +212,6 @@ offset_x = 0
 for im in image:
     img.paste(im, (offset_x, 0))
     offset_x += im.width
-img.save("merged_result04.bmp")
+img.save("merged_result07.bmp")
 img.show()
 
